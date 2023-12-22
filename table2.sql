ae1a7fa66353e4925062a1d90ce2fb65a972d4c6
HeliParmar <142568224+HeliParmar@users.noreply.github.com>
2023-12-22 20:20:03 +0530

    Delete table2.sql

--- table2.sql
+++
@@ -1,6 +0,0 @@
-Create table hotel(name,Id,age,gender,room_no,days);
-Insert into hotel values ('heli',034,20,'F',11,2);
-Insert into hotel values ('saniya',204,19,'F',11,2);
-Insert into hotel values ('Rashida',303,20,'F',11,2);
-Insert into hotel values ('joy',500,19,'M',15,5);
-Insert into hotel values ('dax',501,19,'M',15,5);
