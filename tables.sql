1f63ca227ab9a43d283ada2fc95d355a4bec05b8
HeliParmar <142568224+HeliParmar@users.noreply.github.com>
2023-12-22 20:18:45 +0530

    Add files via upload

---
+++ tables.sql
@@ -0,0 +1,35 @@
+CREATE table home(name,age,height,gender,extra);
+Insert into home values ('Mukesh',51,5.6,'M','senior menber');
+Insert into home values ('Raksha',46,5.5,'F','housewife');
+Insert into home values ('heli',20,5.0,'F','student');
+Insert into home values ('Happy',20,5.4,'M','student');
+Insert into home values ('devika',72,5,'F','grand mother');
+
+Create table orders(id, item, customer_id,section_no,price);
+Insert into orders values(101010,'table',011,8,1000);
+Insert into orders values(120120,'chair',122,1,700);
+Insert into orders values(112112,'clothes',121,4,300);
+Insert into orders values(221221,'boxes',221,3,500);
+Insert into orders values(2121212,'plates',222,6,100);
+
+Create table hotel(name,Id,age,gender,room_no,days);
+Insert into hotel values ('heli',034,20,'F',11,2);
+Insert into hotel values ('saniya',204,19,'F',11,2);
+Insert into hotel values ('Rashida',303,20,'F',11,2);
+Insert into hotel values ('joy',500,19,'M',15,5);
+Insert into hotel values ('dax',501,19,'M',15,5);
+
+
+Create table Dmart(customer_name,Id, product_name, price);
+Insert into Dmart values ('heli',01, 'tshirt',300);
+Insert into Dmart values ('saniya',12, 'sweetshirt' ,500);
+Insert into Dmart values ('Rashida',13, 'jeans',700);
+Insert into Dmart values ('joy',15, 'shirt',400);
+Insert into Dmart values ('dax',16, 'shirt',410);
+
+Create table Bank(name, location,no_of_employees, currency);
+Insert into Bank values('Bank of India', 'ahmedabad',35,'rupees');
+Insert into Bank values('central bank of india','mumbai',50,'rupees');
+Insert into Bank values('canada bank','ahmedabad',50,'rupeesandCAD');
+Insert into Bank values('bank of america','gandhinagar',60,'rupeesandUSD');
+Insert into Bank values('ICIC bank','Ahmedabad',40,'rupees');
