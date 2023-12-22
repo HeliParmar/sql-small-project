2b67fe796cf75fb61fa0091e0e997a212e4448b7
HeliParmar <142568224+HeliParmar@users.noreply.github.com>
2023-12-22 19:51:33 +0530

    Add files via upload

---
+++ table1.sql
@@ -0,0 +1,6 @@
+CREATE table home(name,age,height,gender,extra);
+Insert into home values ('Mukesh',51,5.6,'M','senior menber');
+Insert into home values ('Raksha',46,5.5,'F','housewife');
+Insert into home values ('heli',20,5.0,'F','student');
+Insert into home values ('Happy',20,5.4,'M','student');
+Insert into home values ('devika',72,5,'F','grand mother');
---
+++ table4.sql
@@ -0,0 +1,6 @@
+Create table Bank(name, location,no_of_employees, currency);
+Insert into Bank values('Bank of India', 'ahmedabad',35,'rupees');
+Insert into Bank values('central bank of india','mumbai',50,'rupees');
+Insert into Bank values('canada bank','ahmedabad',50,'rupeesandCAD');
+Insert into Bank values('bank of america','gandhinagar',60,'rupeesandUSD');
+Insert into Bank values('ICIC bank','Ahmedabad',40,'rupees');
