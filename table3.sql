f75a8142cfc60bd282b04b5066a621c766f47ab6
HeliParmar <142568224+HeliParmar@users.noreply.github.com>
2023-12-22 20:28:58 +0530

    Delete table3.sql

--- table3.sql
+++
@@ -1,7 +0,0 @@
-
-Create table Dmart(customer_name,Id, product_name, price);
-Insert into Dmart values ('heli',01, 'tshirt',300);
-Insert into Dmart values ('saniya',12, 'sweetshirt' ,500);
-Insert into Dmart values ('Rashida',13, 'jeans',700);
-Insert into Dmart values ('joy',15, 'shirt',400);
-Insert into Dmart values ('dax',16, 'shirt',410);
